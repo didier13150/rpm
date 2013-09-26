0f87ca250cc4dbd49f673c7f011073bdfacf7f97
didier <dfabert@b2pweb.com>
2013-09-26 16:10:50 +0200

    Update fracplanet with new icons

--- fracplanet/make_icons.sh
+++ fracplanet/make_icons.sh
@@ -2,6 +2,6 @@
 
 for size in 16 22 32 48 64 128 256
 do
-    povray -W${size} -H${size} +Ifracplanet.pov +UA +Ofracplanet-HI${size}.png
+    povray -W${size} -H${size} +Ifracplanet.pov +UA +Ofracplanet-hi${size}.png
 done
 
