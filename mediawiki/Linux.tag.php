7a759a0bc7de5851a2dab80a62e3ffb857fdce2a
didier <dfabert@b2pweb.com>
2013-10-11 10:28:38 +0200

    Add previous tag

--- mediawiki/Linux.tag.php
+++ mediawiki/Linux.tag.php
@@ -10,7 +10,7 @@ function linuxParserInit( Parser $parser ) {
         $parser->setHook( 'package', 'packageRender' );
         $parser->setHook( 'app', 'appRender' );
         $parser->setHook( 'class', 'myClassRender' );
-
+        $parser->setHook( 'previous', 'myPreviousPage' );
         // Always return true from this function. The return value does not denote
         // success or otherwise have meaning - it just must always be true.
         return true;
@@ -25,21 +25,19 @@ function pathRender( $input, array $args, Parser $parser, PPFrame $frame ) {
 }
 
 function packageRender( $input, array $args, Parser $parser, PPFrame $frame ) {
-        // Nothing exciting here, just escape the user-provided
-        // input and throw it back out again
         return '<span style="color:#9d4800;font-weight:bold;">' . htmlspecialchars( $input ) . '</span>';
 }
 
 function appRender( $input, array $args, Parser $parser, PPFrame $frame ) {
-        // Nothing exciting here, just escape the user-provided
-        // input and throw it back out again
         return '<span style="color:#008080;font-weight:bold;">' . htmlspecialchars( $input ) . '</span>';
 }
 
 function myClassRender( $input, array $args, Parser $parser, PPFrame $frame ) {
-        // Nothing exciting here, just escape the user-provided
-        // input and throw it back out again
         return '<span style="color:#0000aa;font-weight:bold;">' . htmlspecialchars( $input ) . '</span>';
+}
+
+function myPreviousPage( $input, array $args, Parser $parser, PPFrame $frame ) {
+        return '<a href="javascript:history.go(-1)">' . htmlspecialchars( $input ) . '</a>';
 }
 
 
